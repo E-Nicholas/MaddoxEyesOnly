5aee9f9b8c4b25057cc33d4a73b97e5879e4b7c8
Eric Nicholas <eric.p.nicholas@gmail.com>
2017-04-09 00:30:08 -0400

    Updates

--- GetTime.m
+++ GetTime.m
@@ -2,14 +2,24 @@ function tvec = GetTime(fs,time,varargin)
 % GetTime will return a time vector with the desired sampling rate and
 % length. Sampling rate is ALWAYS samples per second.
 % If only fs and a length are given returns from t(dt) to t(time).
-% If additional inputs are given they will be processed as follows,
+%
+% If additional inputs are given they will be processed as follows;
+%
 % tvec = GetTime(fs,prezero,postzero,units)
-% UNITS FUNCTIONALITY YET TO BE ADDED
-    dt = 1/fs;
+%
+% Where the second input is the amount of time before 0, in seconds,
+% and postzero is time after zero.
+%
+% A fouth optional input taken as power to set units of tvec (i.e. 3 = ms, 6 = us)
+dt = 1/fs;
 if isempty(varargin)
     tvec = dt:dt:time;
 else
-    tvec = -abs(time):dt:varargin{1};
+    base = 1;
+    if length(varargin) > 1;
+        base = 10^varargin{2};
+    end
+    tvec = -abs(time)*base:dt*base:varargin{1}*base;
 end
 
 end
--- LabEx2.m
+++ LabEx2.m
@@ -9,41 +9,37 @@ load('lab2_data_matlab.mat');
 
 tvec = GetTime(fs,1);
 
+figure('Color',[1 1 1],'NumberTitle','off','Name','First Second of Example Data','Position',scrz);
 plot(tvec,raw(1:length(tvec)));
-
+ylabel('Voltage (V)'); xlabel('Time (s)');
+title('First Second of Example Data');
 %% Starting with Epoching... dem events yo
 
 % Pondering the events, a good many, we are asked if they are structured in
 % any meaningful way... well, let's see.
-howbig = size(events)
-% lots down the first dimension, those are our trials, what are the two
-% values in the second
-peek = events(1:10,:)
-%the first clearly looks like an index position, and the second is our
-%event
 
 %Lets just look at the events in order and see if there's any structure
 figure('Color',[1 1 1],'NumberTitle','off','Name','First 120 events','Position',scrz);
 plot(events(1:120,2))
-set(gca,'XTick',0:5:120,'ylim',[0.75 2.25],'YTick',0:1:2);
+set(gca,'XTick',0:5:120,'ylim',[0.75 2.25],'YTick',0:1:2,'YTickLabel',{'2 - Right Ear' '1 - Left Ear'});
 ylabel('Event Value'); xlabel('Event Number');
 title('The First 120 Event Values');
-% Looks pretty random... we can dig into how often each repeats and to what
-% extent...
-onereps = []; %init
-tworeps = []; %init
+% Looks pretty random from here... but we can dig into how often each
+% repeats and to what extent.
+left_reps = []; %init
+right_reps = []; %init
 prevtrial = events(1,2); %Intialize prevtrial trial tracking variable to track trials
 count = 1; %Initialize counter, all sequences contain at least one stim...
 stepfcn = 1;
 for i = 2:length(events)  %Just quick loop-de-loop to count how many times
-    if events(i,2) == 1   %a trigger value appears in a row each time it
-        if prevtrial == 1 %appears
+    if events(i,2) == 1   %a trigger value appears in a row, and how many
+        if prevtrial == 1 %repititions are present in the
             stepfcn(i) = stepfcn(i-1) + 1;
             count = count + 1;
             prevtrial = events(i,2);
         else
             stepfcn(i) = 1;
-            tworeps = [tworeps count];
+            right_reps = [right_reps count];
             count = 1;
             prevtrial = events(i,2);
         end
@@ -54,50 +50,53 @@ for i = 2:length(events)  %Just quick loop-de-loop to count how many times
             prevtrial = events(i,2);
         else
             stepfcn(i) = -1;
-            onereps = [onereps count];
+            left_reps = [left_reps count];
             count = 1;
             prevtrial = events(i,2);
         end
     end
 end
 
-max(onereps) %Both have a max number of repeats of 7
-max(tworeps)
-
-thresh = 0;
-aboveLine = (stepfcn>=thresh);
-bottomLine = stepfcn;
-topLine = stepfcn;
-bottomLine(aboveLine) = 0;
-topLine(~aboveLine) = 0;
+maxrepeats_ones = max(left_reps) % Both matrices containing the number of sequences for each
+maxrepeats_twos = max(right_reps) % trigger value are the same size 5599 sequences total, and
+                               % both have max values of 7.
+                               
+thresh = 0;                    % To generate a plot showing the sequences
+aboveLine = (stepfcn>=thresh); % of each event type in order, nicely with different
+bottomLine = stepfcn;          % colors for each event value we separate the data
+topLine = stepfcn;             % into two vectors and then set the values of one or
+bottomLine(aboveLine) = 0;     % the other to 0 when the value is above or below thresh
+topLine(~aboveLine) = 0;       % I constructed the stepfcn vector with this plotting method in mind
 
 % The following plot shows the sequence of event repeats, where each point
 % represents the number of repeated events in the last sequence (showing the first 60 sequences).
 figure('Color',[1 1 1],'NumberTitle','off','Name','Repeated Event Sequences (first 60)','Position',scrz.*2);
-subplot(2,1,1)
-plot(onereps(1:200)); hold on; %This plot suggests the number of repeated trials for
-plot(tworeps(1:200)); grid on; %Each trigger value follow a similar structure
-set(gca,'ylim',[0 7],'XTick',0:5:200);  %which also appears fairly random
-legend('Event 1','Event 2','Location','northeast')
+subplot(2,1,1);
+plot(left_reps(1:300),'LineWidth',2); hold on; %This plot suggests a trend for the different trigger values
+plot(right_reps(1:300),'LineWidth',2); grid on; %There seem to be more blue peaks at the sequence number of 6
+set(gca,'ylim',[0 7],'XTick',0:5:300);  %and more red peaks at lower sequence numbers, we need to see the
+legend('Left Ear Click','Right Ear Click','Location','northeast') % relative number of each repeat sequence for both events
 ylabel('Number of Repeats'); xlabel('Sequence Number');
-title('Repeated Event Sequences, overlaid(first 200 of each)');
-subplot(2,1,2)
+title('Repeated Event Sequences, overlaid(first 300 sequences of each event)');
+subplot(2,1,2);
 plot(bottomLine(1:400),'r'); hold on;
 plot(topLine(1:400),'b');
 set(gca,'YTick',-7:1:7,'YTickLabel',{'7' '6' '5' '4' '3' '2' '1' '0' '1' '2' '3' '4' '5' '6' '7'});
 ylabel('Number of Repeats'); xlabel('Sequence Number');
-legend('Event 1','Event 2','Location','northeast')
-title('Repeated Event Sequences, in order (first 60)');
+legend('Right Ear Click','Left Ear Click','Location','northeast');
+title('Repeated Event Sequences, in order (first 400 sequences)');
 
-nreps = unique(onereps);
+nreps = unique(left_reps);
 
 for i = 1:length(nreps)
-    nones_reps(i) = length(find(onereps == nreps(i)));
-    ntwos_reps(i) = length(find(tworeps == nreps(i)));
+    nones_reps(i) = length(find(left_reps == nreps(i)));
+    ntwos_reps(i) = length(find(right_reps == nreps(i)));
 end
 
-%Each trigger value has the same number of repeated sequences
-%across the experiment. So this can't be totally random
+%So, it looks like there's a purposeful structure to the number of repeats
+%of each repeat-sequence for the different event values. Event #2 (right ear) has more
+%sequences with 4 repeated 2's. Event 1 (left ear) has far more repeats of 2, and
+%repeats of 6 consecutive clicks to the left ear.
 figure('Color',[1 1 1],'NumberTitle','off','Name','Number of repeated event sequences','Position',scrz);
 bar3([nones_reps' ntwos_reps']);
 ylabel('Number of Repeats'); xlabel('Event Type'); zlabel('Numer of Sequences');
@@ -105,17 +104,26 @@ ylabel('Number of Repeats'); xlabel('Event Type'); zlabel('Numer of Sequences');
 
 %% Staring With Epoching, now the epoching
 
+tmin = .01;
+tmax = .02;
 
+[tvec, epoch_prebase] = BMEepoch(raw,events,fs,tmin,tmax);
 
+trange = [-0.05 0];
+epoch = BMEbaseline(epoch_prebase,tvec,trange);
 
+event_mean = mean(epoch,3);
+mean_scaled = event_mean .* 10^6;
 
-
-
-
-
-
-
-
-
+tvec = GetTime(fs,tmin,tmax,3); % Get a new time vector with units in ms for plotting
+figure('Color',[1 1 1],'NumberTitle','off','Name','Average ABR Response','Position',scrz); 
+plot(tvec,mean_scaled,'LineWidth',1.5); hold on;
+plot(tvec,zeros(length(tvec)),'--','color',[0.6 0.6 0.6]);
+set(gca,'xlim',[-5 10]);
+ys = get(gca,'ylim');
+plot([0 0],ys,'k--');
+set(gca,'ylim',ys,'XTick',-5:1:10);
+ylabel('Voltage (\muV)'); xlabel('Time (ms)');
+legend('Left Ear Click','Right Ear Click','Location','northeast');
 
 
