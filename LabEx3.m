2b6d96591307dfc6a28e76f9a77c16e90548f240
Eric Nicholas <eric.p.nicholas@gmail.com>
2017-04-13 10:44:21 -0400

    Update

--- LabEx3.m
+++ LabEx3.m
@@ -8,5 +8,29 @@ scrz = get(groot,'ScreenSize')./2;
 load('lab2_data_matlab.mat');
 load('lab3_data_matlab.mat');
 
-tvec = GetTime(1000,2);
+%% Calculating and Plotting Spectra
+f = 150;
+fs = 1000;
+tmax = 2;
+amp = 1;
 
+tvec = GetTime(fs,tmax);
+
+wav = amp*cos(2 * pi * f/fs * tvec * 1000);
+gaussnoise = normrnd(0,1,1,length(tvec));
+noisy_wav = gaussnoise + wav;
+
+dft = fft(noisy_wav);
+P2 = abs(dft/length(noisy_wav));
+
+fvec = fs*(0:length(noisy_wav)-1)/length(noisy_wav);
+
+figure('Color',[1 1 1],'NumberTitle','off','Name','150Hz Cosine With Gaussian Noise','Position',scrz);
+subplot(2,1,1)
+plot(tvec,noisy_wav)
+ylabel('AU'); xlabel('Time (s)');
+title('150Hz Cosine wave with Guassian Noise');
+
+subplot(2,1,2);
+plot(fvec,P2);
+title('Discrete Fourier Transform');
